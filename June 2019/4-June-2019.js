68a5fe8d1e39fcefd79b2b409feee6782846835b
Toyin Dawudu <toyindawudu@gmail.com>
2019-06-04 11:25:45 +0100

    Complete a coding challenge around capacity on a bus

---
+++ June 2019/4-June-2019.js
@@ -0,0 +1,44 @@
+// CHALLENGE NUMBER 1
+
+/*Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+Task Overview:
+You have to write a function that accepts three parameters:
+
+cap is the amount of people the bus can hold excluding the driver.
+on is the number of people on the bus.
+wait is the number of people waiting to get on to the bus.
+If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+*/
+
+function enough(cap, on, wait) {
+  // your code here
+  //create a variable to store the result
+  let result = 0;
+  //subtract the amount of people on the bus from the capacity
+  let remainder = cap - on;
+  //subtract the amount waiting from the remainder 
+  result = wait - remainder;
+  // Return result
+  
+  //create control 
+  
+  if (result < 0){
+    return 0}
+  return result;
+}
+
+/*  Another solution
+
+function enough(cap, on, wait) {
+  if (on + wait < cap){
+  return 0;
+  } else {
+  return (on + wait) - cap;
+  }
+}
+*/
+
+
+// CHALLENGE NUMBER 2
+
