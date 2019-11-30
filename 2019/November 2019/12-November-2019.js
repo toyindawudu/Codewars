d23a9c1bdec59908b73af7c197f71ad907312773
Toyin Dawudu <toyindawudu@gmail.com>
2019-11-30 23:02:49 +0000

    Decided to include my Freecodecamp challenges

--- 2019/November 2019/12-November-2019.js
+++ 2019/November 2019/12-November-2019.js
@@ -55,7 +55,7 @@ function highAndLow(numbers){
   return smallest;
 }
 
-console.log (highAndLow (["1 -9 4 2 7"]));
+//console.log (highAndLow (["1 -9 4 2 7"]));
 
 /* function stringToArray(string){
   // Store the string in a variable 
@@ -68,3 +68,12 @@ console.log (highAndLow (["1 -9 4 2 7"]));
 
 
 // Still working
+
+var arr = [
+  [1,2], [3,4], [5,6]
+];
+for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+    console.log(arr[i][j]);
+  }
+}
---
+++ 2019/November 2019/30-November-2019.js
@@ -0,0 +1,23 @@
+/* 
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2,
+  sayName: function() {return "The name of this duck is " + duck.name + ".";}
+};
+duck.sayName();
+// Returns "The name of this duck is Aflac."
+The example adds the sayName method, which is a function that returns a sentence giving the name of the duck. Notice that the method accessed the name property in the return statement using duck.name. The next challenge will cover another way to do this.
+
+Using the dog object, give it a method called sayLegs. The method should return the sentence "This dog has 4 legs."
+
+*/
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function (){return "This dog has " + dog.numLegs + " legs" + ".";}
+};
+
+console.log (dog.sayLegs());
+
