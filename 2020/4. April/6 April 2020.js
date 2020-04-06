6f5875369a878b73ff4821d73de5036ab9b828ff
Toyin Dawudu <toyindawudu@gmail.com>
2020-04-06 20:21:06 +0100

    Working on a 8 kyu challenge which is incomplete as of this commit

---
+++ 2020/4. April/6 April 2020.js
@@ -0,0 +1,15 @@
+//1. Higherst and Lowest
+
+function highAndLow(numbers){
+  // Initialise variables
+  let sortedNumbers = [];
+  // Split the string in the numbers
+  // Sort string using the .sort() methods
+  sortedNumbers.push() = numbers.sort();
+  // Return the highest and lowest number in the string
+  return (sortedNumbers.length-1, sortedNumbers[0])
+  //
+  // ...
+}
+
+highAndLow("5 4 -4 3 1");
