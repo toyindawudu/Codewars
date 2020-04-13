0299f1caa182797fa886662863f9a4a7ab653d44
Toyin Dawudu <toyindawudu@gmail.com>
2020-04-13 22:12:52 +0100

    Attempted to solve a filtering question but no luck

---
+++ 2020/4. April/13 April 2020.js
@@ -0,0 +1,30 @@
+// List Filtering [Code 7 kyu]
+
+function filter_list(l) {
+  // Create a result for variable
+  //let array = l;
+  let result = [];
+  // create a for loop and iterate through the array and 
+  for (let i = 0; i < l.length; i++){
+    // if (array[i] % 2 === 0) result.push(array[i]);
+     if (typeof array[i] === "number" ) {
+       result.push[array([i]);
+     }
+     return result;
+  };
+  //if it's an integer, push it into a new Array
+  // Return a new array with the strings filtered out
+  //
+}
+
+console.log(filter_list(1,'a','b',0,15));
+
+
+
+// let bigCities = [];
+// for (let i = 0; i < cities.length; i++) {
+//     if (cities[i].population > 3000000) {
+//         bigCities.push(cities[i]);
+//     }
+// }
+// console.log(bigCities);
